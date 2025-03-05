b230a519f7507a4166b3d78bfcc491c0abe6a7c4
Rizal Fadlullah <izzal@MacBook-Air-Rizal.local>
2025-03-05 11:33:29 +0700

    chore: added neardrop

---
+++ Casks/neardrop.rb
@@ -0,0 +1,17 @@
+cask "neardrop" do
+  version "2.1.0"
+  sha "3eaaf874019f9fdef7b7dd0494e7a9005ba81558c0733f931757c9518c6d226b"
+  
+  url "https://github.com/grishka/NearDrop/releases/download/v#{version}/NearDrop.app.zip"
+  name "NearDrop"
+  desc "Airdrop-like file sharing between Android and macOS"
+  homepage "https://github.com/grishka/NearDrop"
+
+  app "NearDrop.app"
+
+  zap trash: [
+    "~/Library/Application Support/NearDrop",
+    "~/Library/Preferences/com.grishka.NearDrop.plist",
+    "~/Library/Saved Application State/com.grishka.NearDrop.savedState"
+  ]
+end
